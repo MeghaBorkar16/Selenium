bec472025e0993fad2e11fae9b9e8759fd8b45ab
meghaborkar165@gmail.com <borkarmegha167@gmail.com>
2021-10-17 05:14:22 +0530

    second commit

--- src/main/java/gitdemo/First_Commit.java
+++ src/main/java/gitdemo/First_Commit.java
@@ -4,7 +4,8 @@ public class First_Commit {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
-		
+		System.out.println("how r u");
+		System.out.println("how r u doing");
 	}
 
 }
